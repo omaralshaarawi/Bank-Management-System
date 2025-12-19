f0e3ce1213a47b9b4352b34a1bffce23452e4d41
Yehia Nader <naderyehia946@gmail.com>
2025-12-19 22:34:56 +0200

    I added struct

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include "sh3ro.c"
+typedef struct
+{
+    int month;
+    int year;
+}date;
+
+typedef struct
+{
+    date open;
+    char name[100];
+    int account_number;
+    float balance;
+    int mobile_number;
+    char email[100];
+ 
+}customer;
+
 
 int main()
 {
