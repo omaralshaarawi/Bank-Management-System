ac48d1ce0d704592ddd733940c044a4e0b0ea3a3
omaralshaarawi <omaralshaarawi88@gmail.com>
2025-12-19 22:46:13 +0200

    added structs file

--- main.c
+++ main.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
 #include "sh3ro.c"
-typedef struct
-{
-    int month;
-    int year;
-}date;
-
-typedef struct
-{
-    date open;
-    char name[100];
-    int account_number;
-    float balance;
-    int mobile_number;
-    char email[100];
- 
-}customer;
-
+#include "structs.c"
 
 int main()
 {
-    int n=8;
-    printf("%d",n);
     
     return 0;
 }
