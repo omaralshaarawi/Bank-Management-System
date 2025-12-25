6265bccf1b6c1a625a6dd94fe17467a1e27fae69
omaralshaarawi <omaralshaarawi88@gmail.com>
2025-12-25 23:53:22 +0200

    test

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main()
         }
     }
 
-   
+   load();
    
     while (1)
     {
--- save.c
+++ save.c
@@ -31,9 +31,17 @@ void save(customer e,int new)
     char acc_number[50];
     sprintf(acc_number,"%lld.txt",e.account_number);
     fptr = fopen(acc_number, "w");
+    if(fptr==NULL)
+    {
+        perror("The account number file wasn't made");
+    }
     fclose(fptr);
     FILE *fptr1;
     fptr1 = fopen("accounts.txt","a");
+    if(fptr1==NULL)
+    {
+        perror("The file wasn't opened");
+    }
     fprintf(fptr1, "\n%lld, %s,%s,%f,%s,%d-%d,%s",e.account_number,e.name,e.email,e.balance,e.mobile_number,e.open.month,e.open.year,e.active);
     fclose(fptr1);
 }
