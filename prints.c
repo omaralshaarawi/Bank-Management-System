d697757bba167bef53fe15cdc2df5a6ce50415d3
omaralshaarawi <omaralshaarawi88@gmail.com>
2025-12-27 16:03:15 +0200

    added report and fixed load

--- prints.c
+++ prints.c
@@ -43,28 +43,115 @@ void report()
 
 }
 
+void print1(int idx)
+{
+    printf("%lld %s %s %f %s %d-%d %s\n",customers[idx].account_number,customers[idx].name,customers[idx].email,customers[idx].balance,customers[idx].mobile_number,customers[idx].open.month,customers[idx].open.year,customers[idx].active);
+}
+
 void SortByDate()
 {
-
+    int sort[n+1];
+    for(int i=0;i<=n;i++)
+    {
+        sort[i]=-1;
+    }
+    for(int j=0;j<=n;j++)
+    {
+        int idx=j;
+        for(int i=0;i<=n;i++)
+        {
+            if(sort[i]==-1)
+            {
+                sort[i]=idx;
+                break;
+            }
+            else if(customers[sort[i]].open.year>customers[idx].open.year||(customers[sort[i]].open.year==customers[idx].open.year&&customers[sort[i]].open.month>customers[idx].open.month))
+            {
+                int temp=sort[i];
+                sort[i]=idx;
+                idx=temp;
+            }
+        }
+    }
+    for(int i=0;i<=n;i++)print1(sort[i]);
 }
 void SortByName()
 {
-    
+    int sort[n+1];
+    for(int i=0;i<=n;i++)
+    {
+        sort[i]=-1;
+    }
+    for(int j=0;j<=n;j++)
+    {
+        int idx=j;
+        for(int i=0;i<=n;i++)
+        {
+            if(sort[i]==-1)
+            {
+                sort[i]=idx;
+                break;
+            }
+            else if(strcmp(customers[sort[i]].name,customers[idx].name)>0)
+            {
+                int temp=sort[i];
+                sort[i]=idx;
+                idx=temp;
+            }
+        }
+    }
+    for(int i=0;i<=n;i++)print1(sort[i]);
 }
 void SortByBalance()
 {
-    
+    int sort[n+1];
+    for(int i=0;i<=n;i++)
+    {
+        sort[i]=-1;
+    }
+    for(int j=0;j<=n;j++)
+    {
+        int idx=j;
+        for(int i=0;i<=n;i++)
+        {
+            if(sort[i]==-1)
+            {
+                sort[i]=idx;
+                break;
+            }
+            else if(customers[sort[i]].balance>customers[idx].balance)
+            {
+                int temp=sort[i];
+                sort[i]=idx;
+                idx=temp;
+            }
+        }
+    }
+    for(int i=0;i<=n;i++)print1(sort[i]);
 }
 void SortByStatus()
 {
-    
+    for(int i=0;i<=n;i++)
+    {
+        if(strcmp(customers[i].active,"inactive")==0)
+        {
+            print1(i);
+        }
+    }
+    for(int i=0;i<=n;i++)
+    {
+        if(strcmp(customers[i].active,"inactive")!=0)
+        {
+            print1(i);
+        }
+    }
 }
 
 void print()
 {
     while(1)
     {
-        printf("How would like to print it: \n-1Sorted by Name\n-2Sorted By Date\n-3Sort by Balance\n-4Sort by Status\n-0 Return back to menu\n");
+        printf("How would like to print it: \n1-Sorted by Name\n2-Sorted By Date\n3-Sort by Balance\n4-Sort by Status\n-0 Return back to menu\n");
         int x;
         if(scanf("%d",&x))
         {
